d3c9840237e4150058b45d97e77207e54a6ebb25
Albin <albinso@kth.se>
2015-12-19 01:48:10 +0100

    TIDelegate class handles behaviour on notification

---
+++ TIDelegate.py
@@ -0,0 +1,39 @@
+import sys, os
+import binascii
+sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__), '/usr/local/lib/python2.7/dist-packages/bluepy/', 'bluepy')))
+import btle
+
+class TIDelegate(btle.DefaultDelegate):
+	def __init__(self, params):
+		btle.DefaultDelegate.__init__(self)
+		self.leftdown = do_nothing
+		self.rightdown = do_nothing
+		self.bothdown = do_nothing
+
+		self.leftup = do_nothing
+		self.rightup = do_nothing
+		self.bothup = do_nothing
+		self.state = 0
+		# ... initialise here
+
+	def handleNotification(self, cHandle, data):
+		data = binascii.b2a_hex(data)
+
+		if data == "00":
+			if self.state == "01":
+				self.leftup()
+			if self.state == "02":
+				self.rightup()
+			if self.state == "03":
+				self.bothup()
+
+		if data == "01":
+			self.leftdown()
+		if data == "02":
+			self.rightdown()
+		if data == "03":
+			self.bothdown()
+		self.state = data
+
+def do_nothing():
+	pass
--- trampz.py
+++ trampz.py
@@ -1,9 +1,18 @@
 import sys, os
+import binascii
+from TIDelegate import TIDelegate
 sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__), '/usr/local/lib/python2.7/dist-packages/bluepy/', 'bluepy')))
 import btle
+
+
+
 
 ADDRESS = "78:A5:04:19:58:E1"
 
 conn = btle.Peripheral(ADDRESS)
+conn.setDelegate(TIDelegate(None))
 
-print(conn)
+while True:
+	if conn.waitForNotifications(1.0):
+		continue
+	print("Waiting for notification")
